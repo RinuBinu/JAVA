9ac20e03011715f57640f4cc36c0f07981a68fc5
RinuBinu <rinupinky2002@gmail.com>
2025-04-11 15:54:55 +0530

    CO4
    CO4

---
+++ calc/arithmetic/Division.java
@@ -0,0 +1,19 @@
+package arithmetic;
+public class Division
+{
+private double num1;
+private double num2;
+public Division(double num1, double num2)
+{
+this.num1 =num1;
+this.num2= num2;
+}
+public double div()
+{
+if (num2 == 0)
+{
+throw new ArithmeticException("Cannot divide by zero");
+}
+return num1 / num2;
+}
+}
